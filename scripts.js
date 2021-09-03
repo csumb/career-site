75dfdf5e6f5a3521959acea283f43491488f820c
codywall <cody.wall@gmail.com>
2021-09-03 10:58:22 -0700

    Fix typo in element selector

--- scripts.js
+++ scripts.js
@@ -1,3 +1,3 @@
 const covidMessage = `Employees are required to comply with the campus’ COVID-19 requirements. Prior to your first day of employment, please visit: <a href="https://csumb.edu/health/coronavirus-information/">https://csumb.edu/health/coronavirus-information/</a> for CSUMB’s most current masking and vaccination requirements.`;
 
-document.getElementById("campaign").innerHTML = covidMessage;
+document.getElementById("campaigns").innerHTML = covidMessage;
