6156c1eb14c88d7d31f194e6db98b43918d9125b
codywall <cody.wall@gmail.com>
2021-09-03 11:05:48 -0700

    Add padding to covid message

--- scripts.js
+++ scripts.js
@@ -1,3 +1,3 @@
-const covidMessage = `Employees are required to comply with the campus’ COVID-19 requirements. Prior to your first day of employment, please visit: <a href="https://csumb.edu/health/coronavirus-information/">https://csumb.edu/health/coronavirus-information/</a> for CSUMB’s most current masking and vaccination requirements.`;
+const covidMessage = `<p style="padding: 1rem;">Employees are required to comply with the campus’ COVID-19 requirements. Prior to your first day of employment, please visit: <a href="https://csumb.edu/health/coronavirus-information/">https://csumb.edu/health/coronavirus-information/</a> for CSUMB’s most current masking and vaccination requirements.</p>`;
 
-document.getElementById("campaigns").innerHTML = covidMessage;
+document.getElementById("campaignsDiv").innerHTML = covidMessage;
