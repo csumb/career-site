2b333f40e7c52811f443b0eb6680d12605f20423
Scott Rush <kind.dog6970@skewedreality.com>
2025-10-23 09:26:35 -0700

    Removed  sentence referencing COVID

--- scripts.js
+++ scripts.js
@@ -1,3 +1,3 @@
-const covidMessage = `<p style="padding: 1rem;">CSUMB’s most current masking and Covid-19 requirements are on our  <a href="https://csumb.edu/health/coronavirus-information/">COVID-19 webpage</a>. For any questions, contact <a href="mailto:humanresources@csumb.edu">humanresources@csumb.edu.</a>
+const covidMessage = `<p style="padding: 1rem;">For any questions, contact <a href="mailto:humanresources@csumb.edu">humanresources@csumb.edu.</a>
 </p>`;
 document.getElementById("campaignsDiv").innerHTML = covidMessage;
